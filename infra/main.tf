0d5affa2cae1b2b9c580fa5da7b2f9831510968a
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-15 14:04:30 -0400

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -27,6 +27,6 @@ resource "docker_image" "gar_image" {
 
   build {
     context    = "infra"
-    dockerfile = "Dockerfile" # file in infra/
+    dockerfile = "infra/Dockerfile" # file in infra/
   }
 }
